30d2b2dfbe2102e7165b5c174bec6b5e0a126e1c
gushi-cookie <gushi-cookie@protonmail.com>
2023-05-20 00:38:01 +0300

    Unify token payload.

--- src/utils/jwt.util.ts
+++ src/utils/jwt.util.ts
@@ -1,6 +1,11 @@
 import jwt from 'jsonwebtoken';
 
-function generateAccessToken(payload: any, expiresIn: string): string {
+export interface TokenPayload {
+    userId: number,
+    nickname: string,
+};
+
+function generateAccessToken(payload: TokenPayload, expiresIn: string): string {
     return jwt.sign(payload, process.env.SECRET_TOKEN as string, { expiresIn });
 };
 
