6e4de5e8d0ee1a31e944c903f50dee3a92cbd459
gushi-cookie <926396z@mail.ru>
2023-05-20 13:36:55 +0300

    Create API for fetching posts with pagination.

---
+++ src/controllers/posts-list.controller.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from 'express';
+import { BlogPost } from '../models/blog-post.model';
+import { User } from '../models/user.model';
+
+
+const PAGINATION = 20;
+
+interface AssocBlogPost extends BlogPost {
+    User: User;
+}
+
+async function fetchList(req: Request, res: Response) {
+    // available query params: page
+    let pageNumber = Number(req.query.page);
+    if(!pageNumber) pageNumber = 0;
+
+    let result = await BlogPost.findAndCountAll({
+        order: ['date'],
+        limit: PAGINATION,
+        offset: PAGINATION * pageNumber,
+        include: User,
+    });
+    if(result.rows.length === 0) return res.status(404).json('List not found.');
+
+    let posts = new Array<AssocBlogPost>();
+    result.rows.forEach(row => {
+        posts.push((row as any));
+    });
+
+    let rawPosts = new Array();
+    posts.forEach(post => {
+        rawPosts.push({
+            id: post.id,
+            userId: post.userId,
+            nickname: post.User.nickname,
+            message: post.message,
+            date: post.date,
+        });
+    });
+
+    res.json({
+        message: 'Success.',
+        posts: rawPosts,
+    });
+};
+
+
+export default {
+    fetchList,
+};
---
+++ src/routes/posts-list.ts
@@ -0,0 +1,8 @@
+import { IRouter, Router } from 'express';
+import postsListController from '../controllers/posts-list.controller';
+
+const router: IRouter = Router();
+
+router.get('/api/posts-list', postsListController.fetchList);
+
+export default router;
