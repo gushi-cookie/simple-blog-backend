82e3213c27199a8d56afd916e5dffe395e76ad71
gushi-cookie <926396z@mail.ru>
2023-05-20 12:31:36 +0300

    Create API for working with posts table.

--- src/routes/blog-post.ts
+++ src/routes/blog-post.ts
@@ -1,16 +1,14 @@
 import { Router, IRouter } from 'express';
 import blogPostController from '../controllers/blog-post.controller';
+import authenticateToken from '../middlewares/authenticateToken';
 
 const router: IRouter = Router();
 
 
-router.post('/api/blog-post', blogPostController.postBlogPost);
-
-router.get('/api/blog-post/:id', blogPostController.fetchBlogPost);
-
-router.delete('/api/blog-post/:id', blogPostController.deleteBlogPost);
-
-router.patch('/api/blog-post/:id', blogPostController.editBlogPost);
+router.get('/api/blog-post/:id(\\d+)', blogPostController.fetchBlogPost);
+router.post('/api/blog-post', authenticateToken, blogPostController.createBlogPost);
+router.patch('/api/blog-post/:id(\\d+)', authenticateToken, blogPostController.editBlogPost);
+router.delete('/api/blog-post/:id(\\d+)', authenticateToken, blogPostController.deleteBlogPost);
 
 
 export default router;
