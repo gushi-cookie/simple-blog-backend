0f01355ca5238a13f60c2c852d4e49b545278776
gushi-cookie <gushi-cookie@protonmail.com>
2023-05-31 16:36:18 +0300

    Modify PATCH: /api/blog-post endpoint.

--- src/routes/blog-post.ts
+++ src/routes/blog-post.ts
@@ -8,7 +8,7 @@ const router: IRouter = Router();
 const upload = multer();
 router.get('/api/blog-post/:id(\\d+)', blogPostController.fetchBlogPost);
 router.post('/api/blog-post', authenticateToken, upload.single('file'), blogPostController.createBlogPost);
-router.patch('/api/blog-post/:id(\\d+)', authenticateToken, blogPostController.editBlogPost);
+router.patch('/api/blog-post/:id(\\d+)', authenticateToken, upload.single('file'), blogPostController.editBlogPost);
 router.delete('/api/blog-post/:id(\\d+)', authenticateToken, blogPostController.deleteBlogPost);
 
 
